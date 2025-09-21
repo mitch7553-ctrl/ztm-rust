c6ec597be128037de4862d845cd67c67650c300f
Mitchell Brown <mitch7553@gmail.com>
2025-09-21 16:01:55 +0800

    pushing code

--- activities/src/bin/a5.rs
+++ activities/src/bin/a5.rs
@@ -4,10 +4,24 @@
 // * Display "1" through "4" in the terminal
 //
 // Notes:
-// * Use a mutable integer variable
-// * Use a loop statement
-// * Print the variable within the loop statement
-// * Use break to exit the loop
 
-fn main() {}
+
+
+
+
+fn main() {
+    // * Use a mutable integer variable
+    let mut i = 1;
+    // * Use a loop statement
+    loop {
+        // * Print the variable within the loop statement
+        println!("{:?}", i);
+        i = i + 1;
+        if i == 5 {
+            // * Use break to exit the loop
+            break;
+        }
+    }
+    println!("We have reached number 4!")
+}
 
