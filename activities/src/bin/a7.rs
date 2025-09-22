f06aef3b311e50776869b89656b5f158993a5c28
Mitchell Brown <mitch7553@gmail.com>
2025-09-23 01:48:36 +0800

    pushing code

--- activities/src/bin/a7.rs
+++ activities/src/bin/a7.rs
@@ -4,11 +4,37 @@
 // * Prints the name of a color to the terminal
 //
 // Notes:
+
+
+
+
 // * Use an enum with color names as variants
+enum Colors {
+    Red,
+    Blue,
+    Green
+}
 // * Use a function to print the color name
 // * The function must use the enum as a parameter
+fn print_colors(which_color: Colors){
 // * Use a match expression to determine which color
-//   name to print
+match which_color {
+    Colors::Red => println!("The color is red."),
+    Colors::Blue => println!("The color is blue."),
+    Colors::Green => println!("The color is green."),
+}
+}
 
-fn main() {}
+
+
+fn main() {
+    //name to print
+
+    print_colors(Colors::Red);
+    print_colors(Colors::Blue);
+    print_colors(Colors::Green);
+
+
+
+}
 
