78f2d659b0cc6092ee7d82123a0b40fa7be792a3
Mitchell Brown <mitch7553@gmail.com>
2025-09-23 15:42:12 +0800

    pushiing code

--- activities/src/bin/a10.rs
+++ activities/src/bin/a10.rs
@@ -5,12 +5,27 @@
 // * Print "its small" if a variable is <= 100
 //
 // Notes:
-// * Use a boolean variable set to the result of
-//   an if..else expression to store whether the value
-//   is > 100 or <= 100
+
 // * Use a function to print the messages
+fn print_message(gt_100: bool) {
 // * Use a match expression to determine which message
 //   to print
+match gt_100{
+    true => println!("It's big."),
+    _=> println!("It's small"),
+}
 
-fn main() {}
+}
+
+
+fn main() {
+    // * Use a boolean variable set to the result of
+//   an if..else expression to store whether the value
+//   is > 100 or <= 100
+let value = 100;
+let is_gt_100 = value > 100;
+print_message(is_gt_100);
+
+
+}
 
