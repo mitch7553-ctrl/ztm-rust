60d2d1947af99763e25e05d85b0420b847d76ef2
Mitchell Brown <mitch7553@gmail.com>
2025-09-23 11:22:04 +0800

    push more code

--- activities/src/bin/a8.rs
+++ activities/src/bin/a8.rs
@@ -9,5 +9,40 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+
+
+// * Use an enum to create different flavors of drinks
+enum Drinks {
+    Grape,
+    Lemon,
+    Strawberry
+}
+// * Use a struct to store drink flavor and fluid ounce information
+struct DrinkInfo {
+    flavor: Drinks,
+    ounce: i32,
+
+}
+// * Use a function to print out the drink flavor and ounces
+fn print_drinks(drink: DrinkInfo) {
+    match drink.flavor {
+        Drinks::Grape => println!("Flavor: Grape"),
+        Drinks::Lemon => println!("Flavor: Lemon"),
+        Drinks::Strawberry => println!("Flavor: Strawberry"),
+    }
+    println!("Ounces: {}", drink.ounce);
+}
+
+
+
+
+fn main() {
+   let drink_selection = DrinkInfo {
+    flavor: Drinks::Grape,
+    ounce: 64,
+   };
+
+ print_drinks(drink_selection);
+
+}
 
