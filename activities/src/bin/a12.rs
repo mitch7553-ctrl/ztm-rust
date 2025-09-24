3f405503e5b2b24cf47c1cf40192dbb5e2b4cee7
Mitchell Brown <mitch7553@gmail.com>
2025-09-24 20:33:43 +0800

    Working in the data collection section

--- activities/src/bin/a12.rs
+++ activities/src/bin/a12.rs
@@ -10,5 +10,41 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+
+
+
+
+
+
+// * Use a struct to encapsulate the box characteristics
+struct ShippingBox {
+    color: Color,
+    weight: f64,
+}
+// * Use an enum for the box color
+enum Color {
+    Red,
+    Blue,
+    Brown,
+}
+// * Implement functionality on the box struct to create a new box
+impl ShippingBox {
+fn newBox() -> Self {
+    color:Color::Red,
+    weight:25.5
+
+}
+
+// * Implement functionality on the box struct to print the characteristics
+fn print_box_type(&self) {
+    println!("Box color: {:?} , box weight: {:?}",self.color, self.weight )
+}
+}
+
+
+
+fn main() {
+    let new = ShippingBox::newBox();
+    new.print_box_type();
+}
 
