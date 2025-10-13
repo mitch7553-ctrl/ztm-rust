04d7f5fdbfd7fef47847bf2959ad073b92ab0421
mitch7553-ctrl <mitch7553@gmail.com>
2025-10-13 22:49:32 +0800

    Finished the data collection model.

--- activities/src/bin/a14.rs
+++ activities/src/bin/a14.rs
@@ -4,12 +4,51 @@
 // * Print out the name and favorite colors of people aged 10 and under
 //
 // Notes:
-// * Use a struct for a persons age, name, and favorite color
-// * The color and name should be stored as a String
-// * Create and store at least 3 people in a vector
+
 // * Iterate through the vector using a for..in loop
 // * Use an if expression to determine which person's info should be printed
+
+// * Use a struct for a persons age, name, and favorite color
+struct Person {
+    age: i32,
+    // * The color and name should be stored as a String
+    name: String,
+    favorite_color: String,
+}
+
 // * The name and colors should be printed using a function
+fn print_info(name: &str, color: &str) {
+    println!("Name: {:?}, Favorite Color: {:?}", name, color);
+}
 
-fn main() {}
+fn main() {
+    // * Create and store at least 3 people in a vector
+    let people = vec![
+        Person {
+            age: 19,
+            name: String::from("John"),
+            favorite_color: String::from("Blue"),
+        },
+        Person {
+            age: 27,
+            name: String::from("Mike"),
+            favorite_color: String::from("Red"),
+        },
+        Person {
+            age: 30,
+            name: String::from("Steve"),
+            favorite_color: String::from("Green"),
+        },
+    ];
 
+    for person in people {
+        if person.age < 25 {
+            print_info(&person.name, &person.favorite_color);
+        } else {
+            println!(
+                "{:?} is over 25 years old. He/she is {:?}.",
+                person.name, person.age
+            );
+        }
+    }
+}
--- activities/src/bin/a4a.rs
+++ activities/src/bin/a4a.rs
@@ -5,17 +5,13 @@
 //
 // Notes:
 
-
-
 fn main() {
-   // * Use a variable set to either true or false
-   let my_match = ;
-   
- // * Use a match expression to determine which message to display
-  match my_match {
-    true => println!("It's true!"),
-    false => println!("It's false!"),
- }
-   
-}
+    // * Use a variable set to either true or false
+    let my_match = true;
 
+    // * Use a match expression to determine which message to display
+    match my_match {
+        true => println!("It's true!"),
+        false => println!("It's false!"),
+    }
+}
