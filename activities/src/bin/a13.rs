0039ac3ead382cf20d9dad772660d587b99056cd
mitch7553-ctrl <mitch7553@gmail.com>
2025-10-13 21:15:59 +0800

    pushing code to git

--- activities/src/bin/a13.rs
+++ activities/src/bin/a13.rs
@@ -5,10 +5,26 @@
 // * Print the total number of elements in a vector
 //
 // Notes:
-// * Use a vector to store 4 numbers
-// * Iterate through the vector using a for..in loop
-// * Determine whether to print the number or print "thirty" inside the loop
-// * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+struct Numbers {
+    values: i32,
+}
 
+fn main() {
+    // * Use a vector to store 4 numbers
+    let my_values = vec![
+        Numbers { values: 10 },
+        Numbers { values: 20 },
+        Numbers { values: 30 },
+        Numbers { values: 40 },
+    ];
+    // * Iterate through the vector using a for..in loop
+    for num in &my_values {
+        // * Determine whether to print the number or print "thirty" inside the loop
+        if num.values == 30 {
+            println!("thirty");
+        }
+    }
+    // * Use the .len() function to print the number of elements in a vector
+    println!("{}", my_values.len());
+}
