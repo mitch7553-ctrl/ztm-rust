1d10102be0be6672bce0b1abb313928edb363e12
Mitchell Brown <mitch7553@gmail.com>
2025-10-15 11:00:42 +0800

    Pushing code.

--- activities/src/bin/a15.rs
+++ activities/src/bin/a15.rs
@@ -7,9 +7,33 @@
 // * All tickets include the price
 //
 // Notes:
-// * Use an enum for the tickets with data associated with each variant
-// * Create one of each ticket and place into a vector
+
+
 // * Use a match expression while iterating the vector to print the ticket info
 
-fn main() {}
+
+
+// * Use an enum for the tickets with data associated with each variant
+#[derive(Debug)]
+enum Tickets {
+    Backstage(f64, String),
+    Vip(f64, String),
+    Standard(f64, String)
+}
+
+
+
+
+fn main() {
+    // * Create one of each ticket and place into a vector
+    let concert = vec![Tickets::Backstage(100.0,"Mike".to_owned()), Tickets::Vip(50.00,"Murphy".to_owned()), Tickets::Standard(25.00, "Max".to_owned())];
+    for tickets in concert {
+        match tickets {
+            Tickets::Backstage(price, holder) => println!("Holder {:?} price: {:?}", price, holder),
+            Tickets::Vip(price, holder) => println!("Holder {:?} price: {:?}", price, holder),
+            Tickets::Standard(price, holder) => println!("Holder {:?} price: {:?}", price, holder),
+        }
+        }
+    }
+
 
