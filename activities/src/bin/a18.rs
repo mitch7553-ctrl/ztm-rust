4609ad4c48d5a0a1bd099624deebe76650337002
Mitchell Brown <mitch7553@gmail.com>
2025-10-27 09:39:52 +0800

    relearning the Result Type

--- activities/src/bin/a18.rs
+++ activities/src/bin/a18.rs
@@ -1,10 +1,10 @@
 // Topic: Result
 //
 // Requirements:
-// * Create an structure named `Adult` that represents a person aged 21 or older:
-//   * The structure must contain the person's name and age
-//   * Implement Debug print functionality using `derive`
-// * Implement a `new` function for the `Adult` structure that returns a Result:
+
+
+
+
 //   * The Ok variant should contain the initialized structure, but only
 //     if the person is aged 21 or older
 //   * The Err variant should contain a String (or &str) that explains why
@@ -15,6 +15,25 @@
 // * Use `match` to print out a message for each `Adult`:
 //   * For the Ok variant, print any message you want
 //   * For the Err variant, print out the error message
+
+
+
+//   * Implement Debug print functionality using `derive`
+// * Create an structure named `Adult` that represents a person aged 21 or older:
+#[derive(Debug)]
+struct Adult {
+    //   * The structure must contain the person's name and age
+  age:u8, 
+  name:String,
+}
+
+// * Implement a `new` function for the `Adult` structure that returns a Result:
+impl Adult {
+fn new( age: u8, name: &str) -> Result<Self, &str> {
+    if age 
+}
+}
+
 
 fn main() {}
 
