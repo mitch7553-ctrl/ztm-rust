2f390dde5f14766e54e7eda78684e666b33e7ed9
Mitchell Brown <mitch7553@gmail.com>
2025-10-28 00:03:54 +0800

    Finishing the rest tomorrow.

--- activities/src/bin/a18.rs
+++ activities/src/bin/a18.rs
@@ -5,8 +5,7 @@
 
 
 
-//   * The Ok variant should contain the initialized structure, but only
-//     if the person is aged 21 or older
+
 //   * The Err variant should contain a String (or &str) that explains why
 //     the structure could not be created
 // * Instantiate two `Adult` structures:
@@ -30,10 +29,30 @@ struct Adult {
 // * Implement a `new` function for the `Adult` structure that returns a Result:
 impl Adult {
 fn new( age: u8, name: &str) -> Result<Self, &str> {
-    if age 
+    //   * The Ok variant should contain the initialized structure, but only
+    //     if the person is aged 21 or older
+    if age >= 21 {
+        Ok(Self {
+            age, 
+            name: name.to_string(),
+        })
+    } else {
+        Err("Age must be at least 21")
+    }
 }
 }
 
 
-fn main() {}
+fn main() {
+    let child = Adult::new(15, "Mike");
+    let adult = Adult::new(21, "Sam");
+    match child {
+        Ok(child) => println!("{} is {} years old", child.name, child.age),
+        Err(e) => println!("{e}"),
+    }
+     match adult {
+        Ok(adult) => println!("{} is {} years old", adult.name, adult.age),
+        Err(e) => println!("{e}"),
+    }
+}
 
